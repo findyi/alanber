8cbe86934f4f3d24f11e5c9142cca1444dc90df2
tufei <tufei8438@gmail.com>
2016-08-07 15:34:08 +0800

    日志输出问题

--- alanber/weixin/__init__.py
+++ alanber/weixin/__init__.py
@@ -52,7 +52,7 @@ class WeixinApi(object):
     def _request(self, method, url, **kwargs):
         logger.debug("请求URL: %s" % url)
         if kwargs.has_key('json'):
-            logger.debug("请求JSON:" + kwargs.get('json'))
+            logger.debug("请求JSON:%s" % kwargs.get('json'))
         r = requests.request(method, url, **kwargs)
         logger.debug("返回响应: %s" % r.text)
         return r
